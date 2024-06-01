b9e858e3c0491ff31ca6654d3ad21248ebe909db
Franco-Sendros <madscientisth@gmail.com>
2024-06-01 15:06:47 -0300

    subi el proyecto master

---
+++ crud/src/main/java/com/api/crud/controllers/UserController.java
@@ -0,0 +1,62 @@
+package com.api.crud.controllers;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.crud.models.UserModel;
+import com.api.crud.services.UserService;
+
+
+
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping
+    public ArrayList<UserModel> getUsers(){
+
+        return this.userService.getUsers();
+    }
+
+    @PostMapping
+    public UserModel saveUser(@RequestBody UserModel user){
+        return this.userService.saveUser(user);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UserModel> getUserById(@PathVariable("id") Long id){
+        return this.userService.getById(id);
+    }
+
+    @PutMapping(path = "/{id}")
+    public UserModel updateById(@RequestBody UserModel request,@PathVariable("id") Long id){
+        return this.userService.updateById(request, id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String deleteUser(@PathVariable("id") Long id){
+        Boolean ok = this.userService.deleteUser(id);
+        if (ok) {
+            return "User whith id " + id + " has be deleted!";
+        }else{
+            return "Error : ni modo pa.";
+        }
+    }
+    
+}
---
+++ crud/src/main/java/com/api/crud/models/UserModel.java
@@ -0,0 +1,57 @@
+package com.api.crud.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_user;
+
+    @Column(name = "firstname")
+    private String firstName;
+
+    @Column(name = "lastname")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    public long getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(long id) {
+        this.id_user = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+
+
+}
